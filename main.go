bf82e966727b5e6ce0bdbd8decb26099404150ed
Patrick Oyarzun <patrickoyarzun@gmail.com>
2017-02-19 17:51:17 -0500

    simplification

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Logiraptor/realtalk/interp"
+)
+
+func printObject(obj Object) {
+	fmt.Println(formatObject(obj))
+}
+
+func formatObject(obj Object) string {
+	return string(obj.RespondTo(Message{
+		Token: "to_s",
+	}).(StringType))
+}
+
+func main() {
+	object := interp.NewRTClass("Object", nil)
+	object.Methods.AddMethod("print", interp.LiteralExpression{
+		Value: interp.RTInt(5),
+	})
+
+	instance := interp.NewRTObject(object)
+	fmt.Println(instance.Send("print"))
+}
